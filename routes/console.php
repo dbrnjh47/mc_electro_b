c6b1d233fca42a17d3402bdc0c52631cc111c99d
Victor <shulgasv1ls74.ru@mail.ru>
2025-03-14 17:17:46 +0500

    add cron

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,17 @@
 <?php
 
+use Barryvdh\Debugbar\Twig\Extension\Debug;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote');
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::call(function () {
+    Log::debug("test log");
+})
+    ->name('Обновление курсов')
+    ->everyMinute();
+    // ->hourly();
