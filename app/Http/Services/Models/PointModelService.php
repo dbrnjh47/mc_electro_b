325f42fa64f49740bee14df210eb384787cacf05
Victor <shulgasv1ls74.ru@mail.ru>
2025-04-28 15:48:20 +0500

    test join

--- app/Http/Services/Models/PointModelService.php
+++ app/Http/Services/Models/PointModelService.php
@@ -9,7 +9,13 @@ class PointModelService
     public $pagination = 2;
     public function defult()
     {
-        return Point::where("is_on", 1)->with('links.category')->with('phones')->with('photos')->with('locale')->whereHas('locale');
+        return Point::select((new Point())->getTable().'.*')
+        ->where("is_on", 1)
+        ->with('links.category')
+        ->with('phones')
+        ->with('photos')
+        ->with('locale')
+        ->whereHas('locale');
     }
     public function get()
     {
@@ -18,7 +24,10 @@ class PointModelService
 
     public function pagination()
     {
-        return $this->defult()->paginate($this->pagination);
+        return $this->defult()
+            ->join("points_ru as locale_ru", 'points.id', '=', 'locale_ru.point_id')
+            ->where('locale_ru.title', 'LIKE', "%тес2т%")
+            ->paginate($this->pagination);
     }
 
     public function find($id)
