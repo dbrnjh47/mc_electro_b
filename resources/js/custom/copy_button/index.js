eba8a63fcf8ef844fa86e2d684ded3a64c0ec030
Victor <shulgasv1ls74.ru@mail.ru>
2025-07-16 12:14:25 +0500

    add function copy text

--- resources/js/custom/copy_button/index.js
+++ resources/js/custom/copy_button/index.js
@@ -1,2 +1,16 @@
 import "./index.scss";
 
+$(".copy_button").click(function () {
+    copyToClipboard(this);
+});
+
+function copyToClipboard(obj) {
+    const $temp = $('<input>');
+    $("body").append($temp);
+
+    obj = $(obj);
+    let textToCopy = (obj.data("copy-text") !== undefined ? obj.data("copy-text") : obj.text());
+    $temp.val(textToCopy).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
